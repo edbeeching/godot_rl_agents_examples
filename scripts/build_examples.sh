de7da6afedef68b3f04699ba3aa36adb1a1a27bf
Edward Beeching <edbeeching@gmail.com>
2022-12-20 14:33:01 +0100

    updated build script

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -2,11 +2,10 @@
 GODOT_BINARY=/home/edward/play/godot/Godot_v4.0-beta4_linux.x86_64
 mkdir builds
 EXAMPLE_DIRS=$(ls examples)
-EXAMPLE_DIRS=(BallChase)
 for EXAMPLE in $EXAMPLE_DIRS; do
-    echo $EXAMPLE
-    cp -r addons  examples/$EXAMPLE/addons
-    # build for linux
+    echo "Building $EXAMPLE"
+    #cp -r addons  examples/$EXAMPLE/addons
     mkdir -p builds/$EXAMPLE/
-    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ builds/$EXAMPLE/$EXAMPLE.x86_64
+    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ ../../builds/$EXAMPLE/$EXAMPLE.x86_64
+    $GODOT_BINARY --headless --export "Windows Desktop" --path examples/$EXAMPLE/ ../../builds/$EXAMPLE/$EXAMPLE.exe
 done
