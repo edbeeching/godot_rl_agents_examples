1c80c007c565bb6c362a13df2609d31f60d21b61
Edward Beeching <edbeeching@gmail.com>
2022-12-20 16:34:26 +0100

    added to hub support

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 GODOT_BINARY=/home/edward/play/godot/Godot_v4.0-beta4_linux.x86_64
-mkdir builds
+
 EXAMPLE_DIRS=$(ls examples)
 for EXAMPLE in $EXAMPLE_DIRS; do
     echo "Building $EXAMPLE"
-    #cp -r addons  examples/$EXAMPLE/addons
-    mkdir -p builds/$EXAMPLE/
-    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ ../../builds/$EXAMPLE/$EXAMPLE.x86_64
-    $GODOT_BINARY --headless --export "Windows Desktop" --path examples/$EXAMPLE/ ../../builds/$EXAMPLE/$EXAMPLE.exe
+
+    mkdir examples/$EXAMPLE/build
+    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ build/$EXAMPLE/$EXAMPLE.x86_64
+    $GODOT_BINARY --headless --export "Windows Desktop" --path examples/$EXAMPLE/ build/$EXAMPLE/$EXAMPLE.exe
+    python scripts/example_to_hub.py  --hf_repository="edbeeching/godot_rl_${EXAMPLE}" --dir_path="examples/${EXAMPLE}/" --env_name=$EXAMPLE
 done
---
+++ scripts/example_to_hub.py
@@ -0,0 +1,93 @@
+import argparse
+import json
+import os
+import sys
+from huggingface_hub import HfApi, Repository, repocard, upload_file, upload_folder
+
+
+def generate_model_card(
+    dir_path: str, 
+    env: str,
+    repo_id: str,
+):
+    readme_path = os.path.join(dir_path, "README.md")
+    repo_name = repo_id.split("/")[1]
+
+    readme = f"""
+A RL environment called {env} for the Godot Game Engine.\n
+This environment was created with: https://github.com/edbeeching/godot_rl_agents \n
+
+## Downloading the environment \n
+After installing Godot RL Agents, download the environment with: \n
+```
+gdrl.load_from_hub -r {repo_id}
+```
+\n
+
+"""
+
+    with open(readme_path, "w", encoding="utf-8") as f:
+        f.write(readme)
+
+    metadata = {}
+    metadata["library_name"] = "godot-rl"
+    metadata["tags"] = [
+        "deep-reinforcement-learning",
+        "reinforcement-learning",
+        "godot-rl",
+        "environments",
+        "video-games"
+    ]
+    repocard.metadata_save(readme_path, metadata)
+
+def push_to_hf(dir_path: str, repo_name: str):
+    repo_url = HfApi().create_repo(
+        repo_id=repo_name,
+        private=False,
+        exist_ok=True,
+    )
+
+    upload_folder(
+        repo_id=repo_name,
+        folder_path=dir_path,
+        path_in_repo=".",
+        ignore_patterns=[".git/*"],
+    )
+
+
+# def load_from_hf(dir_path: str, repo_id: str):
+#     temp = repo_id.split("/")
+#     repo_name = temp[1]
+
+#     local_dir = os.path.join(dir_path, repo_name)
+#     Repository(local_dir, repo_id)
+#     log.info(f"The repository {repo_id} has been cloned to {local_dir}")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--hf_repository",
+        help="The full repo_id to push to on the HuggingFace Hub. Must be of the form <username>/<repo_name>",
+        type=str,
+    )
+    parser.add_argument(
+        "--dir_path",
+        help="The path of the environment",
+        default="./",
+        type=str,
+    )
+    parser.add_argument(
+        "--env_name",
+        help="The name of the environment",
+        default="unknown",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    generate_model_card(args.dir_path, args.env_name, args.hf_repository)
+    push_to_hf(args.dir_path, args.hf_repository)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
