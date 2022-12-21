5d01598d0d54453d7882dcb5e87797c60fdf0d04
Edward Beeching <edbeeching@gmail.com>
2022-12-21 14:08:36 +0100

    updates build examples

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -5,8 +5,8 @@ EXAMPLE_DIRS=$(ls examples)
 for EXAMPLE in $EXAMPLE_DIRS; do
     echo "Building $EXAMPLE"
 
-    mkdir examples/$EXAMPLE/build
-    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ build/$EXAMPLE/$EXAMPLE.x86_64
-    $GODOT_BINARY --headless --export "Windows Desktop" --path examples/$EXAMPLE/ build/$EXAMPLE/$EXAMPLE.exe
+    mkdir examples/$EXAMPLE/bin
+    $GODOT_BINARY --headless --export "Linux/X11" --path examples/$EXAMPLE/ bin/$EXAMPLE.x86_64
+    $GODOT_BINARY --headless --export "Windows Desktop" --path examples/$EXAMPLE/ bin/$EXAMPLE.exe
     python scripts/example_to_hub.py  --hf_repository="edbeeching/godot_rl_${EXAMPLE}" --dir_path="examples/${EXAMPLE}/" --env_name=$EXAMPLE
 done
