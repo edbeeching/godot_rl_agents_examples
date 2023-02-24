d980b40c2a3ea538bd0356336724fd0e335bcb6d
visuallization <fr@ovos.at>
2023-02-24 12:40:55 +0100

    chore(build script): move GODOT_BINARIES into .env file

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-GODOT_BINARY=/home/edward/play/godot/Godot_v4.0-beta4_linux.x86_64
-#GODOT_BINARY=/Applications/Godot.app/Contents/MacOS/Godot
+if [ -e .env ] ;
+then
+    set -a; source .env; set +a
+else
+    echo ".env file is missing. Please run 'cp example.env .env' in the project's root directory."
+    exit 0
+fi
 
-EXAMPLE_DIRS=$(ls examples)
-for EXAMPLE in $EXAMPLE_DIRS; do
-    echo "Building $EXAMPLE"
+if [ -z "$GODOT_ENGINE" ] ;
+then
+    echo "GODOT_ENGINE is not set. Please set GODOT_ENGINE with the path to your godot engine binaries in the .env file."
+else
+    EXAMPLE_DIRS=$(ls examples)
+    for EXAMPLE in $EXAMPLE_DIRS; do
+        echo "Building $EXAMPLE"
 
-    mkdir examples/$EXAMPLE/bin
-    $GODOT_BINARY --headless --export-release "Linux/X11" --path examples/$EXAMPLE/ bin/$EXAMPLE.x86_64
-    $GODOT_BINARY --headless --export-release "Windows Desktop" --path examples/$EXAMPLE/ bin/$EXAMPLE.exe
-    $GODOT_BINARY --headless --export-release "macOS" --path examples/$EXAMPLE/ bin/$EXAMPLE.app
-    python scripts/example_to_hub.py  --hf_repository="edbeeching/godot_rl_${EXAMPLE}" --dir_path="examples/${EXAMPLE}/" --env_name=$EXAMPLE
-done
+        mkdir examples/$EXAMPLE/bin
+        $GODOT_ENGINE --headless --export-release "Linux/X11" --path examples/$EXAMPLE/ bin/$EXAMPLE.x86_64
+        $GODOT_ENGINE --headless --export-release "Windows Desktop" --path examples/$EXAMPLE/ bin/$EXAMPLE.exe
+        $GODOT_ENGINE --headless --export-release "macOS" --path examples/$EXAMPLE/ bin/$EXAMPLE.app
+        python scripts/example_to_hub.py  --hf_repository="edbeeching/godot_rl_${EXAMPLE}" --dir_path="examples/${EXAMPLE}/" --env_name=$EXAMPLE
+    done
+fi
