c25275ca38bf2e30498d588cdeb7d7b65dd6aa91
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2023-06-20 08:41:10 +0200

    Update SessionConfigurator.cs
    Updated the name of the ComputeName enum.
    Changed the check for "OSX" to also use the new enum format.

--- examples/BallChase/addons/godot_rl_agents/onnx/csharp/SessionConfigurator.cs
+++ examples/BallChase/addons/godot_rl_agents/onnx/csharp/SessionConfigurator.cs
@@ -19,7 +19,8 @@ namespace GodotONNX
             SystemCheck();
             return options;
         }
-        public enum ComputeNames
+
+        public enum ComputeName
         {
             CUDA,
             ROCm,
@@ -27,6 +28,7 @@ namespace GodotONNX
             CoreML,
             CPU
         }
+
         /// <include file='docs/SessionConfigurator.xml' path='docs/members[@name="SessionConfigurator"]/SystemCheck/*'/>
         static public void SystemCheck()
         {
@@ -36,7 +38,7 @@ namespace GodotONNX
             //December 2022: CUDA is not working. 
 
             string OSName = OS.GetName(); //Get OS Name
-            ComputeNames ComputeAPI = ComputeCheck(); //Get Compute API
+            ComputeName ComputeAPI = ComputeCheck(); //Get Compute API
                                                         //TODO: Get CPU architecture
 
             //Linux can use OpenVINO (C#) on x64 and ROCm on x86 (GDNative/C++)
@@ -51,35 +53,34 @@ namespace GodotONNX
             switch (OSName)
             {
                 case "Windows": //Can use CUDA, DirectML
-                    if (ComputeAPI is ComputeNames.CUDA)
+                    if (ComputeAPI is ComputeName.CUDA)
                     {
                         //CUDA 
                         //options.AppendExecutionProvider_CUDA(0);
                         options.AppendExecutionProvider_DML(0);
                     }
-                    else if (ComputeAPI is ComputeNames.DirectML)
+                    else if (ComputeAPI is ComputeName.DirectML)
                     {
                         //DirectML
                         options.AppendExecutionProvider_DML(0);
                     }
                     break;
                 case "X11": //Can use CUDA, ROCm
-                    if (ComputeAPI is ComputeNames.CUDA)
+                    if (ComputeAPI is ComputeName.CUDA)
                     {
                         //CUDA
                         //options.AppendExecutionProvider_CUDA(0);
                     }
-                    if (ComputeAPI is ComputeNames.ROCm)
+                    if (ComputeAPI is ComputeName.ROCm)
                     {
                         //ROCm, only works on x86 
                         //Research indicates that this has to be compiled as a GDNative plugin
                         GD.Print("ROCm not supported yet, using CPU.");
                         options.AppendExecutionProvider_CPU(0);
                     }
-
                     break;
                 case "OSX": //Can use CoreML
-                    if (ComputeAPI == 0)
+                    if (ComputeAPI is ComputeName.CoreML)
                     { //CoreML
                       //TODO: Needs testing
                         options.AppendExecutionProvider_CoreML(0);
@@ -93,7 +94,7 @@ namespace GodotONNX
         }
         /// <include file='docs/SessionConfigurator.xml' path='docs/members[@name="SessionConfigurator"]/ComputeCheck/*'/>
 
-        public static ComputeNames ComputeCheck()
+        public static ComputeName ComputeCheck()
         {
             string adapterName = Godot.RenderingServer.GetVideoAdapterName();
             //string adapterVendor = Godot.RenderingServer.GetVideoAdapterVendor();
@@ -101,19 +102,19 @@ namespace GodotONNX
             //TODO: GPU vendors for MacOS, what do they even use these days?
             if (adapterName.Contains("INTEL"))
             {
-                return ComputeNames.DirectML;
+                return ComputeName.DirectML;
             }
             if (adapterName.Contains("AMD"))
             {
-                return ComputeNames.DirectML;
+                return ComputeName.DirectML;
             }
             if (adapterName.Contains("NVIDIA"))
             {
-                return ComputeNames.CUDA;
+                return ComputeName.CUDA;
             }
 
             GD.Print("Graphics Card not recognized."); //Should use CPU
-            return ComputeNames.CPU;
+            return ComputeName.CPU;
         }
     }
 }
