4ea00c81e6fdaeb00d401b2b8bb457bd64a9f2ca
Edward Beeching <edbeeching@gmail.com>
2023-08-11 09:36:40 +0200

    adds chest collectable

---
+++ examples/GridSensor3DExample/chest.gd
@@ -0,0 +1,17 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	if body is Player:
+		body.chest_collected()
+		queue_free()
