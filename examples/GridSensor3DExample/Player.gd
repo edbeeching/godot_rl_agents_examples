25fec0089a2bf5bce250ef56a330f2c64e2ba0c2
Edward Beeching <edbeeching@gmail.com>
2023-08-21 09:02:29 +0200

    adds GridSensor3D example

--- examples/GridSensor3DExample/Player.gd
+++ examples/GridSensor3DExample/Player.gd
@@ -1,19 +1,18 @@
 extends CharacterBody3D
 class_name Player
 
-const SPEED = 5.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+const TURN_SENS = 2.0
 
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var fb = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	var turn = Input.get_action_strength("turn_left") - Input.get_action_strength("turn_right")
+	rotation.y += deg_to_rad(turn*TURN_SENS)
+	var direction = (transform.basis * Vector3(0.0, 0, fb)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
