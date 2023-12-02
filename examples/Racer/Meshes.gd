1ae759cbdb0af7d8241ac6ed838bf64ed76f4bc0
Metin Çelik <metincelik88@gmail.com>
2023-12-03 00:25:39 +0100

    Use AIController in Racer

--- examples/Racer/Meshes.gd
+++ examples/Racer/Meshes.gd
@@ -1,15 +1,12 @@
 extends Node3D
 
 
-
-func set_mesh(name):
-	var node = get_node(name)
+func set_mesh(mesh_name):
+	var node = get_node(mesh_name)
 	if node == null:
 		print("mesh not found")
 		return
 	for child in get_children():
 		child.set_visible(false)
-		
+
 	node.set_visible(true)
-		
-	
