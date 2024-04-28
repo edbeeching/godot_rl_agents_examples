31f30ac5405887c54c679be597c695fecdc092d8
LorinczAdrien <lorinczadrienjeno@gmail.com>
2024-04-28 23:28:59 +0300

    Updated SharpAll project with C# AIController implementation

--- examples/TestExamples/SimpleReachGoal/CSharpAll/scenes/player/Player.cs
+++ examples/TestExamples/SimpleReachGoal/CSharpAll/scenes/player/Player.cs
@@ -9,7 +9,7 @@ public partial class Player : CharacterBody3D
     [Export]
     public Area3D Obstacle { get; set; }
     [Export]
-    public Node3D AIController { get; set; }
+    public PlayerAIController AIController { get; set; }
     [Export]
     public float Speed { get => speed; set => speed = value; }
     public Vector2 RequestedMovement { get; set; }
@@ -31,11 +31,11 @@ public partial class Player : CharacterBody3D
 
         // If controlled by human, takes the keyboard arrows as input
         // otherwise, requested_movement will be set by the AIController based on RL agent's output actions
-        if ((string)AIController.Get("heuristic") == "human")
+        if (AIController.heuristic == "human")
         {
             RequestedMovement = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
         }
-
+        
         RequestedMovement = RequestedMovement.LimitLength(1f) * Speed;
 
         Velocity = new(RequestedMovement.X, Velocity.Y, RequestedMovement.Y);
@@ -45,7 +45,7 @@ public partial class Player : CharacterBody3D
         // This is related to the sync node Action Repeat functionality,
         // we don't get a new action for every physics step. This check may
         // not be required for every env, and is not used in all examples.
-        if (!(bool)AIController.Get("done"))
+        if (!AIController.done)
         {
             MoveAndSlide();
         }
@@ -66,15 +66,14 @@ public partial class Player : CharacterBody3D
     // Ends the game, setting an optional reward
     private void GameOver(double reward)
     {
-        double currentReward = (double)AIController.Get("reward");
-        AIController.Set("reward", currentReward + reward);
+        AIController.reward += (float)reward;
         GameSceneManager.Reset();
     }
 
     // Resets the player and AIController
     public void Reset()
     {
-        AIController.Call("end_episode");
+        AIController.EndEpisode();
         Transform = initialTransform;
     }
 
