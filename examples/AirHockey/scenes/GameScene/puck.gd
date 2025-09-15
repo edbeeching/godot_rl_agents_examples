aed5c3ee434ecaa4edd520cbd7d348a1485db9f7
Ivan-267 <61947090+Ivan-267@users.noreply.github.com>
2025-09-15 08:20:14 +0200

    Updates airhockey

--- examples/AirHockey/scenes/GameScene/puck.gd
+++ examples/AirHockey/scenes/GameScene/puck.gd
@@ -3,13 +3,16 @@ class_name Puck
 
 var _initial_position: Vector3
 
+
 func _ready():
 	_initial_position = position
 	reset()
 
+
 func _on_goal_body_entered(_body):
 	reset()
 
+
 func reset():
-	position = _initial_position + (Vector3.RIGHT * randf_range(-1.2,1.2))
+	position = _initial_position + (Vector3.RIGHT * randf_range(-1.2, 1.2))
 	linear_velocity = Vector3.ZERO
